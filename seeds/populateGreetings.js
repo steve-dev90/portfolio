e6703eacd8dbc7d7da7ea2f3d02ed475278ee72f
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-06-07 21:50:25 +1200

    heroku bug fix

--- seeds/populateGreetings.js
+++ seeds/populateGreetings.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('projects').insert([
         {id: 1, title: "ShopBro2.0", image: 'shopbrovid.mp4', copy: JSON.stringify([
-          { para: "ShopBro2.0 was my final group project at bootcamp. " },
-          { para: "Front end technologies used include React.js, Redux.js, " }
+          { para: "ShopBro2.0 was my final group project at bootcamp. The application helps people keep under budget when they are shopping. It also keeps track of past shopping spends." },
+          { para: "Front end technologies used include React.js, Redux.js, and bulma. For the backend we used node.js, express.js, postgres, and knex.js. The team also started to implement the project as a mobile app using React Native." }
         ])},
         {id: 2, title: "Square of Pointlessness", image: 'square.mp4', copy: JSON.stringify([
-          { para: "Square of Pointlessness was my " }
+          { para: "Square of Pointlessness was my final exercise for bootcamp prep. I used native Javascript to animate a square." }
         ])}
 
       ])
