dca4f95a54528cd7f5e3c3b5babb6055136174db
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-06-18 12:21:10 +1200

    working on ezyme testing

--- test/client/About.test.js
+++ test/client/About.test.js
@@ -1,34 +1,59 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow, mount, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
 
-import {About} from '../../client/components/About'
+
+import About from '../../client/components/About'
 import './setup-dom'
+import { showAbout, hideAbout } from '../../client/actions/portfolio'
 
 import {Provider} from 'react-redux'
 
-import configureStore from  'redux-mock-store'
+import configureStore from 'redux-mock-store'
 
 const mockStore = configureStore([])
 
-jest.mock('../../client/actions/greetings.js', () => ({
-  getGreetings: () => ({
+jest.mock('../../client/actions/portfolio.js', () => ({
+  showAbout: () => ({
+    type: 'FAKE_ACTION'
+  }),
+  hideAbout: () => ({
     type: 'FAKE_ACTION'
   })
+
 }))
 
-test('Greetings.jsx with no greetings', () => {
-  const store = mockStore()
+test('About.jsx buttons are working', () => {
+  const store = mockStore({
+    portfolio: [{aboutDisplay: true}]
+  })
 
-  const wrapper = shallow(<Greetings
-    greetings={[]}
-    dispatch={store.dispatch}
-  />)
-
-  expect(wrapper.find('button').text()).toBe('Show Greetings')
+  const wrapper = mount (
+    <Provider store={store}>
+      <About />
+    </Provider>
+  )
 
   wrapper.find('button').simulate('click')
-  expect(store.getActions()).toHaveLength(1)
+
   expect(store.getActions()[0]).toEqual({
     type: 'FAKE_ACTION'
   })
+})
+
+test('About.jsx content is present', () => {
+  const store = mockStore({
+    portfolio: {aboutDisplay: true}
+  })
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <About />
+    </Provider>
+  )
+
+  expect(wrapper.find('h2').first().text()).toBe('About Me')
+  expect(wrapper.find('div').children('p').first().text().includes('My name is Steve Torrens.')).toBe(true)
+
 })
