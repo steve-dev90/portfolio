ae4e808be688814a9e4282d6d44a395fccbfe3bd
Steve Torrens <stevetorrens70@gmail.com>
2019-05-25 19:10:01 +1200

    Tested about

--- test/client/components/About.test.js
+++ test/client/components/About.test.js
@@ -1,58 +1,16 @@
 import React from 'react'
-import { shallow, mount, configure } from 'enzyme'
+import { shallow } from 'enzyme'
 import './enzymeSetup'
 import About from '../../../client/components/About'
-import './setup-dom'
-import { showAbout, hideAbout } from '../../../client/actions/portfolio'
 
-import {Provider} from 'react-redux'
+describe('About', () => {
+  const wrapper = shallow(<About />)
 
-import configureStore from 'redux-mock-store'
-
-const mockStore = configureStore([])
-
-// jest.mock('../../client/actions/portfolio.js', () => ({
-//   showAbout: () => ({
-//     type: 'FAKE_ACTION'
-//   }),
-//   hideAbout: () => ({
-//     type: 'FAKE_ACTION'
-//   })
-
-// }))
-
-// test('About.jsx buttons are working', () => {
-//   const store = mockStore({
-//     portfolio: [{aboutDisplay: true}]
-//   })
-
-//   const wrapper = mount (
-//     <Provider store={store}>
-//       <About />
-//     </Provider>
-//   )
-
-//   wrapper.find('button').simulate('click')
-
-//   expect(store.getActions()[0]).toEqual({
-//     type: 'FAKE_ACTION'
-//   })
-// })
-
-test('About.jsx content is present', () => {
-  const store = mockStore({
-    portfolio: {aboutDisplay: true}
+  it('renders the `Who I am` title', () => {
+    expect(wrapper.find('h2').first().text()).toBe('Who I am')
   })
 
-  const wrapper = mount(
-    <Provider store={store}>
-      <About />
-    </Provider>
-  )
-
-  console.log(wrapper)
-
-  expect(wrapper.find('h2').first().text()).toBe('Who I am')
-  expect(wrapper.find('div').children('p').first().text().includes('My name is Steve Torrens')).toBe(true)
-
+  it('renders the Who I am paragraph correctly', () => {
+    expect(wrapper.find('div').children('p').first().text().includes('My name is Steve Torrens')).toBe(true)
+  })
 })
--- test/client/components/LandingContent.test.js
+++ test/client/components/LandingContent.test.js
@@ -8,10 +8,10 @@ describe('LandingContent', () => {
 
   it('renders the `What I do` title', () => {
     expect(wrapper.find('h2').text()).toEqual('What I do')
-  });
+  })
 
   it('renders a `/about` link', () => {
-    expect(wrapper.find('Link').prop('to')).toEqual('./about');
-  });
+    expect(wrapper.find('Link').prop('to')).toEqual('./about')
+  })
 
 })
